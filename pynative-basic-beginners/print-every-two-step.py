133b1ec0fd905212a447580d985ca67238ea0a02
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-07-04 22:45:16 +0200

    new eco

---
+++ pynative-basic-beginners/print-every-two-step.py
@@ -0,0 +1,2 @@
+# Write a program to accept a string from the user and display characters that are present at an even index number.
+# For example, str = "pynative" so you should display ‘p’, ‘n’, ‘t’, ‘v’.
