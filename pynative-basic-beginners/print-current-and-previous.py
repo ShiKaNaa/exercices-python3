a56ac8b0fb68494b08aedb7b9a1bb9730d54a2ad
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-07-04 22:42:03 +0200

    not working

--- pynative-basic-beginners/print-current-and-previous.py
+++ pynative-basic-beginners/print-current-and-previous.py
@@ -1,8 +1,6 @@
 # Write a program to iterate the first 10 numbers and in each iteration, print the sum of the current and previous number.
 
-previous = 0
-
 for i in range(10):
-    previous -= i
-    next = i + 1
-    print(f'Current number is {i}, previous number is {previous}, next is {next}')
+    previous = i - 1
+    sum_with_next = i + i + 1
+    print(f'Current number is {i}, previous number is {previous}, next is {sum_with_next}')
