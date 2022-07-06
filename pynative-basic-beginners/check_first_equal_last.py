16cf70c6bf69f4c50efa9c94a99ca3ac457df4a8
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-07-06 17:56:55 +0200

    new exo

---
+++ pynative-basic-beginners/check_first_equal_last.py
@@ -0,0 +1,8 @@
+# Write a function to return True if the first and last number of a given list is same.
+# If numbers are different then return False.
+
+# Given list: [10, 20, 30, 40, 10]
+# result is True
+
+# numbers_y = [75, 65, 35, 75, 30]
+# result is False
