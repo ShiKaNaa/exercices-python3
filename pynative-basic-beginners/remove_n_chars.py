aed19f1b26f09a4fcb969adfb83e258a565d2b60
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-07-06 17:51:38 +0200

    fixed typo

--- pynative-basic-beginners/remove_n_chars.py
+++ pynative-basic-beginners/remove_n_chars.py
@@ -2,7 +2,7 @@
 # remove_chars("pynative", 4) so output must be tive. Here we need to remove first four characters from a string.
 
 def remove_chars(string_to_change, how_much):
-    return string_to_change[:how_much]
+    return string_to_change[how_much:]
 
 string_to_change = input("please wright a word: ")
 how_much = int(input("how much do you want to remove from the start: "))
