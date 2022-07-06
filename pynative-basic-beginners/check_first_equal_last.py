514fcbfa15f230128376ad46aa0e6c7878e19139
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-07-06 17:59:24 +0200

    new exo

--- pynative-basic-beginners/check_first_equal_last.py
+++ pynative-basic-beginners/check_first_equal_last.py
@@ -6,3 +6,9 @@
 
 # numbers_y = [75, 65, 35, 75, 30]
 # result is False
+
+def first_equal_last(array_of_int):
+    return True if array_of_int[0] == array_of_int.last else False
+
+first_equal_last([75, 65, 35, 75, 30])
+first_equal_last([10, 20, 30, 40, 10])
