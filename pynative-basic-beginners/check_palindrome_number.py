f2e3da92ec389b70b310f253000480410e5f955a
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-07-07 10:07:34 +0200

    nex exo

--- pynative-basic-beginners/check_palindrome_number.py
+++ pynative-basic-beginners/check_palindrome_number.py
@@ -1,3 +1,9 @@
 # Write a program to check if the given number is a palindrome number.
 # A palindrome number is a number that is same after reverse.
 # For example 545, is the palindrome numbers
+
+def check_if_palindrome(user_number):
+    return True
+
+user_input = int(input("please enter a number: "))
+print(check_if_palindrome(user_input))
