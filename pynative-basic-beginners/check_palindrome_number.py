3cfa8429cf9d2646143d2a05057e331e3f5b5f65
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-07-07 10:09:11 +0200

    nex exo

--- pynative-basic-beginners/check_palindrome_number.py
+++ pynative-basic-beginners/check_palindrome_number.py
@@ -5,5 +5,11 @@
 def check_if_palindrome(user_number):
     return True
 
-user_input = int(input("please enter a number: "))
-print(check_if_palindrome(user_input))
+# user_input = int(input("please enter a number: "))
+print(check_if_palindrome(454))
+print(check_if_palindrome(13331))
+print(check_if_palindrome(1661))
+print(check_if_palindrome(14599541))
+
+print(check_if_palindrome(1231))
+print(check_if_palindrome(16429815641))
