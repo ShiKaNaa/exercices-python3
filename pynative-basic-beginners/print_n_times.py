270cdfb87143ec6f72c8117177ca9e418c9fd59f
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-07-07 09:55:32 +0200

    new exo

--- pynative-basic-beginners/print_n_times.py
+++ pynative-basic-beginners/print_n_times.py
@@ -2,3 +2,10 @@
 # 2 2
 # 3 3 3
 # 4 4 4 4
+
+def print_n_times(number_to_print):
+    range_from_number = range(0..number_to_print)
+    print(number_to_print)
+
+num = int(input("Please enter a number: "))
+print(print_n_times(num))
